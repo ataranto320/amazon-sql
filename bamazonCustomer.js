510fb757bcff14cf4382a43551545eeaa2f36d8d
Austin T <austin.taranto@gmail.com>
2019-07-18 23:27:07 -0400

    tried different code which didnt seem to work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -97,16 +97,33 @@ function searchStock(){
             message: "How many units of the product would you like?"
         })
         .then(function(answer){
-            var query = "UPDATE products SET stock_quantity = ?"
+            var query = "UPDATE products SET stock_quantity = ? WHERE id = ?"
             // var query = "UPDATE stock_quantity FROM products WHERE stock_quntity = answer.userInput"
             // var sql = "UPDATE products SET completed ? WHERE stock_quantity ?"
-            connection.query(query, [ answer.userInput ], function(err, res){
+            connection.query(query, [ answer.userInput ], {itme_id}, function(err, res){
             // connection.query(query, {"UPDATE stock_quantity FROM products WHERE stock_quantity ?"}, function(err, res){
                 if (err) throw err;
                 // for (var i = 0; i < res.length; i++){
                 //     console.log(userInput)
                 // }
                 console.log(res);
+
+                // var query = connection.query(
+                //     "UPDATE products SET stock_quantity = ? WHERE id = ?",
+                //     [
+                //         {
+                //             quantity: 200
+                //         },
+                //         {
+                //             item_id: 7
+                //         }
+                //     ],
+                //     fucntion(err, res), {
+                //         if (err) {throw err};
+                //         console.log(res);
+                //     }
+                // )
+                
                 runSearch();
             });
         });
