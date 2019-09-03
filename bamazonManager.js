82b22f10d63983d15dbc16b7a0c716a72c0097dc
Austin T <austin.taranto@gmail.com>
2019-09-02 22:22:54 -0400

    added add new product function

--- bamazonManager.js
+++ bamazonManager.js
@@ -103,6 +103,12 @@ function addInv() {
 // }
 
 function addNewProduct() {
-    connection.query("INSERT INTO products")
+    connection.query("INSERT INTO products [item_id, product_name, department_name, price, stock_quantity] VALUES [?, ?, ?, ?, ?"),
+    function(err, res) {
+        if (err) throw err;
+        managerList();
+    };
 }
+
+
 
